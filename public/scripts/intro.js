3216a33860e7da82f10be40cb78ec242517918be
Alcilene Reis <reis.alcilene@gmail.com>
2020-06-06 11:12:55 -0300

    First commit

---
+++ public/scripts/intro.js
@@ -0,0 +1,69 @@
+// Comments
+// document.wride("Hello")
+
+// Variáveis, tipos de dados
+// let myvar = "He"
+// const myconst = "He"
+
+// document.write(myconst +myvar)
+
+// string
+// "Isso é uma string"
+// 'Isso também é uma string'
+// `Isso é uma string também`
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true or false
+// const bTrue = True
+// const bFalse = False
+// document.write(bFalse)
+
+// objetos possuem propriedades e funcionalidades
+//  const pessoa = {
+//    altura: "1,80m",
+//    idade: 24,
+//    solteiro: true,
+//    correr(){
+//      document.write("executar uma grande lógica de correr")
+//    }
+//  }
+ 
+// pessoa.correr()
+
+// Array - Vetores
+
+// const colecaoDeBolinhas = ["blue", "green", 1, {name:"My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+
+// function sayMyName(name) {
+//   document.write(name)
+// }
+
+// // executar
+// sayMyName("Douglas")
+// sayMyName("Valeska")
+// sayMyName("Kyam")
+
+// condicionais
+
+// const notaFinal = 7
+
+// if( notaFinal < 5) {
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+//loop repetições
+
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
+  
